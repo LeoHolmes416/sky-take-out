faf520dda3fc8663211d86ca5d8777b6f7057c7c
yoooo <1084213040@qq.com>
2024-03-12 11:12:35 +0800

    第十三次提交：完善新增菜品功能（添加口味信息）

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -36,9 +36,15 @@ public class DishServiceImpl implements DishService {
         BeanUtils.copyProperties(dishDTO,dish); //对象属性拷贝
         //向菜品表插入一条数据
         dishMapper.insert(dish);
+
+        //获取insert语句生成的主键值，即菜品id
+        Long dishId = dish.getId();
         //向口味表中插入n条数据
         List<DishFlavor> flavors = dishDTO.getFlavors();  //菜品信息中提取出口味信息 list内包括 dishId,name,value
         if(flavors != null && flavors.size() > 0 ){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);   //遍历循环插入菜品id
+            });
             //向口味表中插入n条数据 (batch 批量插入)
             dishFlavorMapper.insertBatch(flavors);
 
